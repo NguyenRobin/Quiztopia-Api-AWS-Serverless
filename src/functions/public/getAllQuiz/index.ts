28488ed8c5791bff51300002aa79da1159773900
NguyenRobin <robin.nnguyen@hotmail.com>
2023-09-27 17:27:00 +0200

    feat: getAllQuiz in progress

--- src/functions/public/getAllQuiz/index.ts
+++ src/functions/public/getAllQuiz/index.ts
@@ -1,12 +1,41 @@
+import { ScanCommand } from '@aws-sdk/client-dynamodb';
+import { db } from '../../../services/db';
+import middy from '@middy/core';
 import { APIGatewayProxyEvent } from 'aws-lambda';
-interface SignupRequest {
-  email: string;
-  password: string;
+import { sendBodyResponse, sendErrorResponse } from '../../../responses';
+
+export async function getAllQuiz() {
+  const command = new ScanCommand({
+    TableName: 'Quiztopia',
+    FilterExpression: 'begins_with(PK, :PK) and begins_with(SK, :SK)',
+    ExpressionAttributeValues: {
+      ':PK': { S: 'quizCategory#' },
+      ':SK': { S: 'quiz#' },
+    },
+  });
+
+  try {
+    const { Items } = await db.send(command);
+    const quizTopics = Items?.map((quiz) => {
+      return {
+        quizCategory: quiz.QuizName.S,
+        creator: quiz.Creator.S,
+      };
+    });
+    return quizTopics;
+  } catch (error) {
+    throw error;
+  }
 }
-export const handler = async function name(event: APIGatewayProxyEvent) {
-  console.log(event);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ event }),
-  };
-};
+
+async function lambda(event: APIGatewayProxyEvent) {
+  try {
+    const allQuiz = await getAllQuiz();
+    console.log(allQuiz);
+    return sendBodyResponse(200, { result: allQuiz?.length, quiz: allQuiz });
+  } catch (error) {
+    return sendErrorResponse(error);
+  }
+}
+
+export const handler = middy(lambda);
